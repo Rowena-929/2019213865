97fedbb670a6ee44845fef5cb9b3732adbbb5f30
Rowena-929 <82950087+Rowena-929@users.noreply.github.com>
2022-01-10 08:29:36 +0800

    Add files via upload

---
+++ 02/my.js
@@ -0,0 +1,149 @@
+var col1 = document.getElementsByClassName("col1");
+        var col2 = document.getElementsByClassName("col2");
+        var col3 = document.getElementsByClassName("col3");
+        var col4 = document.getElementsByClassName("col4");
+        var col5 = document.getElementsByClassName("col5");
+
+        var detail = document.getElementsByClassName("a");
+        col1[0].onmouseover = function(){
+            detail[0].innerHTML = "教室：n217 </br>教师：姚华雄</br>周数：1-19周"
+            detail[0].className = "detail";
+            detail = document.getElementsByClassName("detail");
+        }
+        col1[0].onmouseout = function(){
+            detail[0].innerHTML = '';
+            detail[0].className = 'a';
+            detail = document.getElementsByClassName("a");
+        }
+
+        col1[1].onmouseover = function(){
+            detail[0].innerHTML = "教室：n112 </br>教师：李增阳</br>周数：1-19周"
+            detail[0].className = "detail";
+            detail = document.getElementsByClassName("detail");
+        }
+        col1[1].onmouseout = function(){
+            detail[0].innerHTML = '';
+            detail[0].className = 'a';
+            detail = document.getElementsByClassName("a");
+        }
+
+        col2[0].onmouseover = function(){
+            detail[0].innerHTML = "教室：n520 </br>教师：涂新辉</br>周数：1-18周双周"
+            detail[0].className = "detail";
+            detail = document.getElementsByClassName("detail");
+        }
+        col2[0].onmouseout = function(){
+            detail[0].innerHTML = '';
+            detail[0].className = 'a';
+            detail = document.getElementsByClassName("a");
+        }
+
+        col2[1].onmouseover = function(){
+            detail[0].innerHTML = "教室：8506 </br>教师：江娟</br>周数：1-9周"
+            detail[0].className = "detail";
+            detail = document.getElementsByClassName("detail");
+        }
+        col2[1].onmouseout = function(){
+            detail[0].innerHTML = '';
+            detail[0].className = 'a';
+            detail = document.getElementsByClassName("a");
+        }
+
+        col2[2].onmouseover = function(){
+            detail[0].innerHTML = "教室：n108 </br>教师：张茂元</br>周数：1-19周"
+            detail[0].className = "detail";
+            detail = document.getElementsByClassName("detail");
+        }
+        col2[2].onmouseout = function(){
+            detail[0].innerHTML = '';
+            detail[0].className = 'a';
+            detail = document.getElementsByClassName("a");
+        }
+
+        col3[0].onmouseover = function(){
+            detail[0].innerHTML = "教室：n520 </br>教师：张连发</br>周数：1-19周"
+            detail[0].className = "detail";
+            detail = document.getElementsByClassName("detail");
+        }
+        col3[0].onmouseout = function(){
+            detail[0].innerHTML = '';
+            detail[0].className = 'a';
+            detail = document.getElementsByClassName("a");
+        }
+
+        col3[1].onmouseover = function(){
+            detail[0].innerHTML = "教室：n213 </br>教师：喻莹</br>周数：1-19周"
+            detail[0].className = "detail";
+            detail = document.getElementsByClassName("detail");
+        }
+        col3[1].onmouseout = function(){
+            detail[0].innerHTML = '';
+            detail[0].className = 'a';
+            detail = document.getElementsByClassName("a");
+        }
+
+        col3[2].onmouseover = function(){
+            detail[0].innerHTML = "教室：n535 </br>教师：姚华雄</br>周数：1-19周单周"
+            detail[0].className = "detail";
+            detail = document.getElementsByClassName("detail");
+        }
+        col3[2].onmouseout = function(){
+            detail[0].innerHTML = '';
+            detail[0].className = 'a';
+            detail = document.getElementsByClassName("a");
+        }
+
+        col4[0].onmouseover = function(){
+            detail[0].innerHTML = "教室：n528 </br>教师：张茂元</br>周数：1-18周双周"
+            detail[0].className = "detail";
+            detail = document.getElementsByClassName("detail");
+        }
+        col4[0].onmouseout = function(){
+            detail[0].innerHTML = '';
+            detail[0].className = 'a';
+            detail = document.getElementsByClassName("a");
+        }
+
+        col4[1].onmouseover = function(){
+            detail[0].innerHTML = "教室：n223 </br>教师：吕惠东</br>周数：1-19周"
+            detail[0].className = "detail";
+            detail = document.getElementsByClassName("detail");
+        }
+        col4[1].onmouseout = function(){
+            detail[0].innerHTML = '';
+            detail[0].className = 'a';
+            detail = document.getElementsByClassName("a");
+        }
+
+        col4[2].onmouseover = function(){
+            detail[0].innerHTML = "教室：n108 </br>教师：涂新辉</br>周数：1-19周"
+            detail[0].className = "detail";
+            detail = document.getElementsByClassName("detail");
+        }
+        col4[2].onmouseout = function(){
+            detail[0].innerHTML = '';
+            detail[0].className = 'a';
+            detail = document.getElementsByClassName("a");
+        }
+
+        col5[0].onmouseover = function(){
+            detail[0].innerHTML = "教室：n524 </br>教师：喻莹</br>周数：1-19周"
+            detail[0].className = "detail";
+            detail = document.getElementsByClassName("detail");
+        }
+        col5[0].onmouseout = function(){
+            detail[0].innerHTML = '';
+            detail[0].className = 'a';
+            detail = document.getElementsByClassName("a");
+        }
+
+        col5[1].onmouseover = function(){
+            detail[0].innerHTML = "教室：3216 </br>教师：邹心胜</br>周数：1-19周"
+            detail[0].className = "detail";
+            detail = document.getElementsByClassName("detail");
+        }
+        col5[1].onmouseout = function(){
+            detail[0].innerHTML = '';
+            detail[0].className = 'a';
+            detail = document.getElementsByClassName("a");
+        }
